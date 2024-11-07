7e411da5087ab71f6d73110cceaa85181023e11a
Satish12883 <117818345+Satish12883@users.noreply.github.com>
2024-11-07 00:43:22 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// function displayPdf(pdfUrl){
+//     var pdfId=document.getElementById("pdfViewr");
+//     if (pdfId) {
+//         pdfId.src = pdfUrl;
+//     } else {
+//         console.error("PDF iframe not found!");
+//     }
+
+// }
+// const pdfUrl = 'files/Nextcloud Manual.pdf';
+// displayPdf(pdfUrl);
